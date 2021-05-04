c8a2f624d10b879531695dc18f451a7b9f1cf28c
Minh Tran <wisekingtran@gmail.com>
2021-05-04 13:49:51 +0700

    Build side bar

--- digital-saler/src/App.tsx
+++ digital-saler/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import {
+  Configurations,
+  Order,
+  Overview,
+  Revenue,
+  Users,
+  History,
+} from "./pages/Overviews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to refresh.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path="/overview" component={Overview} exact></Route>
+        <Route path="/overview/users" component={Users} exact></Route>
+        <Route path="/overview/revenue" component={Revenue} exact></Route>
+        <Route path="/order" component={Order} exact></Route>
+        <Route path="/history" component={History} exact></Route>
+        <Route path="/configurations" component={Configurations} exact></Route>
+      </Switch>
+    </Router>
   );
 }
 
